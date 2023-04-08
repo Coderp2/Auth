fc5469c77dd45abcfa1f4f8bd7dd1cfa4e25ae99
Coderp2 <pavnyaat1997@gmail.com>
2023-04-08 19:41:05 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-// mongoose.set('strictQuery', true);
+
+// mongoose.set('strictQuery', false);
+// mongoose.connect("mongodb+srv://Codep2:Codep2@cluster0.hnoxbmp.mongodb.net/Auth_page", { useNewUrlParser: true,  useUnifiedTopology: true, family: 4, });
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true,  useUnifiedTopology: true, family: 4, });
 
 const db = mongoose.connection;
